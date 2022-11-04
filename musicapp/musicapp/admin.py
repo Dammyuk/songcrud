0cf73c3810458f3d75d230e2b8018b751b03f6c2
Dammyuk <116426790+Dammyuk@users.noreply.github.com>
2022-11-04 13:13:57 +0100

    Delete musicapp/musicapp directory

--- musicapp/musicapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Artist, Lyric,Song
-admin.site.register(Artist)
-admin.site.register(Song)
-admin.site.register(Lyric)
